bb60cac0798f298a91950a0a09c52e05b7b12db3
Akene Uzezi <xtremejoexj@gmail.com>
2026-06-03 12:46:41 +0100

    Add health route

--- cmd/routes.go
+++ cmd/routes.go
@@ -8,6 +8,16 @@ import (
 
 func (a *application) routes() http.Handler {
 	g := gin.Default()
+	g.RedirectTrailingSlash = true
+
+	v1 := g.Group("/api/v1")
+	{
+		v1.GET("/ping", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+	}
 
 	return  g
 }
