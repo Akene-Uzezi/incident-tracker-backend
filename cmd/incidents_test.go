e3ceb8a15cd22ca33c41fe5f5bbbcec46ba55a87
Akene-Uzezi <xtremejoexj@gmail.com>
2026-07-09 13:15:58 +0100

    add test for testing reporting incidents

--- cmd/incidents_test.go
+++ cmd/incidents_test.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"issueTracking/internal/db"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestReportIncident(t *testing.T) {
@@ -20,4 +25,40 @@ func TestReportIncident(t *testing.T) {
 	r := gin.Default()
 
 	r.POST("/api/v1/incidents", a.reportIncident)
+
+	payload := map[string]any{
+		"principalName":       "testName",
+		"princidpalGender":    "Male",
+		"principalDob":        "today",
+		"principalType":       "Patient",
+		"patientId":           "iajdaj232",
+		"patientWardDept":     "icu",
+		"peopleInvolved":      "peopleInvolved",
+		"dateOfIncident":      "today",
+		"timeOfIncident":      "now",
+		"locationOfIncident":  "here",
+		"incidentWardDept":    "here?",
+		"isNearMiss":          false,
+		"causeGroup":          "causeGroup",
+		"reporterName":        "Akene Uzezi",
+		"reporterDesignation": "???",
+		"signature":           true,
+		"reporterInfo":        "some info",
+		"date":                "today",
+		"severityLevel":       "minor",
+		"incidentStatus":      "unresolved",
+	}
+	jsonBody, _ := json.Marshal(&payload)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/api/v1/incidents", bytes.NewBuffer(jsonBody))
+
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	var response map[string]any
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "testName", response["principalName"])
 }
