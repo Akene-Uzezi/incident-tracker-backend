e9b5f3909fbd9802eaeb4a16dc701bdf61c7b53e
Akene-Uzezi <xtremejoexj@gmail.com>
2026-07-09 17:27:17 +0100

    finish writing test update success

--- cmd/users_test.go
+++ cmd/users_test.go
@@ -13,6 +13,7 @@ import (
 )
 
 func TestUpdateUnauthorized(t *testing.T) {
+	db.TruncateTables(t, testPool)
 	gin.SetMode(gin.TestMode)
 
 	a := &application{
@@ -44,4 +45,26 @@ func TestUpdateSuccess(t *testing.T) {
 	}
 	err := insertUser(a, t)
 	assert.NoError(t, err)
+
+	payload := &UpdateRequest{
+		Name:       "testuser",
+		Email:      "testuser@example.com",
+		Role:       "manager",
+		Department: "gopc",
+	}
+	jsonBody, _ := json.Marshal(&payload)
+
+	r := gin.Default()
+	r.PUT("/api/v1/update", mockAuthMiddleware("superadmin"), a.update)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("PUT", "/api/v1/update", bytes.NewBuffer(jsonBody))
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	var response map[string]any
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "manager", response["role"])
 }
