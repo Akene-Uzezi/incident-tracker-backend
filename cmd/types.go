2f3263b0920a400bc42b0382e11ba4f8b2b56aef
Akene-Uzezi <xtremejoexj@gmail.com>
2026-07-16 12:50:54 +0100

    add function to check if password was passed if not use default password for user

--- cmd/types.go
+++ cmd/types.go
@@ -9,7 +9,7 @@ import (
 type RegisterRequest struct {
 	Name       string `json:"name" binding:"required"`
 	Email      string `json:"email" binding:"required"`
-	Password   string `json:"password" binding:"required,min=8"`
+	Password   string `json:"password" binding:"omitempty,min=8"`
 	Role       string `json:"role" binding:"required"`
 	Department string `json:"department" binding:"required"`
 }
